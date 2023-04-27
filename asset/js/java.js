45ba05fbcdec90eac1165c6722f220c578272755
Nathalie Moraga <natie.moraga.sch@gmail.com>
2023-04-27 00:33:40 -0400

    Final

--- asset/js/java.js
+++ asset/js/java.js
@@ -4,5 +4,4 @@ var carousel = new bootstrap.Carousel(carusel1, {
   interval:50,
   wrap: false
 
-
 })
--- asset/js/jquery.js
+++ asset/js/jquery.js
@@ -1,42 +1,28 @@
 $(document).ready(function () {
+
     $("#enviar").click(function () {
         alert("el correo fue enviado exitosamente...");
     });
 
-    $(document).ready(function(){
-        $('[data-toggle="tooltip"]').tooltip();   
-      });
+    $(document).ready(function () {
+        $('#plane').popover({ title: "Viajes", content: "Conoce nuestros viajes, destinos, hoteles, etc", trigger: "hover" });
+    });
+    $(document).ready(function () {
+        $('#montaña').popover({ title: "Atracciones", content: "landscapes, parques naturales, flora-fauna autoctona, lagos y mucho más", trigger: "hover" });
+    });
+    $(document).ready(function () {
+        $('#ruta').popover({ title: "Rutas", content: "Conoce tus rutas, recorridos, travel tips, equipo necesario", trigger: "hover" });
+    });
 
-      $(document).ready(function(){
-        $("button").click(function(){
-          $(this).hide();
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $(document).ready(function () {
+        $("button").click(function () {
+            $(this).hide();
         });
 
-
-    $("h2").click(function () { $(".car-tex").toggle(2000); });
-
-    $("h4").on('dblclick', function () {
-        $("#INGREDIENTES").toggleClass("text-danger");
+        $("#Destacados").on('dblclick', function () {
+            $("#DestacadosS").toggleClass("text-danger")
+        });
     });
-    $("h4").on('dblclick', function () {
-        $("#preparacion").toggleClass("text-danger");
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
