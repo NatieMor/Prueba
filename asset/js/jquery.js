f8244319c8f4a020c83e6eb1307e2e2d7f78f145
Nathalie Moraga <natie.moraga.sch@gmail.com>
2023-04-28 01:19:33 -0400

    ultimos cambios

--- asset/js/jquery.js
+++ asset/js/jquery.js
@@ -1,35 +1,19 @@
 $(document).ready(function () {
+
+    $(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
     $("#enviar").click(function () {
-        alert("el correo fue enviado exitosamente...");
+        alert("El correo fue enviado exitosamente, nos pondremos en contacto pronto...");
     });
 
-    $(document).ready(function () {
-        $('#plane').popover({ title: "Viajes", content: "Conoce nuestros destinos, lugares, ciudades, hoteles y mucho más", trigger: "hover", placement: "top" });
-    });
-    $(document).ready(function () {
-        $('#ruta').popover({ title: "Rutas", content: "Conce las rutas que tenemos para ti, senderos,", trigger: "hover", placement: "top" });
-    });
-    $(document).ready(function () {
-        $('#montaña').popover({ title: "lugares", content: "landscapes, playas, lagos, bosques y toda la naturaleza que quieras", trigger: "hover", placement: "bottom" });
-    });
-    $(document).ready(function(){
-        $('[data-toggle="tooltip"]').tooltip({placement="top"});   
-      });
-  
-      
+    $('#plane').popover({ content: "Somos viajeros a destinos increibles y que nos motiva a compartir lo glorioso de nuestro país", placement: "top", trigger: "hover" });
 
+    $('#montaña').popover({ content: "Somos amantes de lo natural y la belleza de nuestro territorio", placement: "bottom", trigger: "hover" });
 
+    $('#ruta').popover({ content: "Somos aventureros de las rutas más hermosas de nuestros parques nacionales", placement: "top", trigger: "hover" });
 
-
-
-
-
-
-
-
-
-
-
-
+    //fin de javaS// 
 
 });
--- java.js
+++
@@ -1,6 +0,0 @@
-var carousel = new bootstrap.Carousel(carusel1)
-
-var carousel = new bootstrap.Carousel(carusel1, {
-  interval:50,
-  wrap: false
-})
